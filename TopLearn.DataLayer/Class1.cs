bf0b07bd7fed5d42b22ffc431eba6553b508bea7
amirhossin-mortezaee <mortezaeeamirhoussin@gmail.com>
2024-09-11 11:46:18 +0330

    ایجاد پروژه و تنظیمات اولیه

--- TopLearn.DataLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TopLearn.DataLayer
-{
-    public class Class1
-    {
-    }
-}
--- TopLearn.Web/Startup.cs
+++ TopLearn.Web/Startup.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,25 +11,11 @@ namespace TopLearn.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,15 +25,12 @@ namespace TopLearn.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
+            app.UseMvcWithDefaultRoute();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
